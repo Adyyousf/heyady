ea24ab26de3a6383fc79be6e7640a96b646f6fd6
AdyYousf <adyyousf197@gmail.com>
2021-07-08 23:08:12 +0530

    edit

--- Mountaincoders/settings.py
+++ Mountaincoders/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!ikk+!9^c4$7f3-b%bvj1e(n2iv46m%3pgo0l9cx^gtpbbz4_%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['heyady.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
